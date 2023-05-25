3d4bbce6c3ab09eece140917cc15adcf2a6aa3a3
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2023-05-25 16:09:47 +0800

    change format of fis.sql

--- faculty-information-system-master/fis.sql
+++ faculty-information-system-master/fis.sql
@@ -9,15 +9,6 @@ CREATE TABLE `roles` (
   `role_id` INT PRIMARY KEY,
   `role_name` VARCHAR(50) NOT NULL 
 );
-
--- Inserting roles
-INSERT INTO `roles` (`role_id`, `role_name`)
-VALUES
-  (1, 'Admin'),
-  (2, 'Faculty'),
-  (3, 'Guest'),
-  (4, 'Students');
-
 -- Add an index on the role_name column in the roles table
 ALTER TABLE `roles` ADD INDEX `idx_role_name` (`role_name`);
 
@@ -30,6 +21,88 @@ CREATE TABLE `users` (
   FOREIGN KEY (`role_name`) REFERENCES `roles`(`role_name`)
 );
 
+-- Table structure for table `staff`
+CREATE TABLE `staff` (
+  `StaffNo` varchar(12) NOT NULL,
+  `Password` varchar(128) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Table structure for table `student`
+CREATE TABLE `student` (
+  `StudentNo` varchar(11) NOT NULL,
+  `Password` varchar(32) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `department` (
+  `DepartmentCode` varchar(256) NOT NULL,
+  `DepartmentName` varchar(256) NOT NULL,
+  `Email` varchar(256) NOT NULL,
+  `Phone` varchar(256) NOT NULL,
+  `Location` TEXT NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Table structure for table `Faculty`
+CREATE TABLE `faculty` (
+  `FacultyID` varchar(10) NOT NULL,
+  `FacultyName` varchar(256) NOT NULL,
+  `Position` varchar(20) NOT NULL,
+  `Department` varchar(20) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `PhoneNumber` varchar(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `workHistory` (
+  `FacultyName` varchar(256) NOT NULL,
+  `CompanyName` varchar(256) NOT NULL,
+  `JobTitle` varchar(20) NOT NULL,
+  `StartDate` DATE NOT NULL,
+  `EndDate` DATE,
+  `Description` TEXT
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `publications` (
+  `Title` varchar(256) NOT NULL,
+  `PublicationType` varchar(256) NOT NULL,
+  `PublicationDate` DATE NOT NULL,
+  `Author` varchar(256) NOT NULL,
+  `AuthorType` varchar(256) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `degrees` (
+  `FacultyName` varchar(256) NOT NULL,
+  `Degree` varchar(256) NOT NULL,
+  `DateAttained` DATE NOT NULL,
+  `Institution` varchar(256) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `grantsAwards` (
+  `FacultyName` varchar(256) NOT NULL,
+  `GrantsAwards` varchar(256) NOT NULL,
+  `DateAttained` DATE NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `courses` (
+  `CourseCode` varchar(256) NOT NULL,
+  `CourseName` varchar(256) NOT NULL,
+  `FacultyName` varchar(256) NOT NULL,
+  `Department` varchar(256) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Table structure for table `faq`
+CREATE TABLE `faq` (
+  `faqID` int(10) NOT NULL,
+  `faqHeading` varchar(256) NOT NULL,
+  `faqContent` varchar(1024) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Inserting roles
+INSERT INTO `roles` (`role_id`, `role_name`)
+VALUES
+  (1, 'Admin'),
+  (2, 'Faculty'),
+  (3, 'Guest'),
+  (4, 'Students');
+
 -- Inserting sample users
 INSERT INTO `users` (`user_id`, `username`, `password`, `role_name`)
 VALUES
@@ -39,44 +112,12 @@ VALUES
   (4, 'Student', '12345', 'Students'),
   (5, 'Student2', '12345', 'Students');
 
--- --------------------------------------------------------
---
--- Table structure for table `faq`
---
-CREATE TABLE `faq` (
-  `faqID` int(10) NOT NULL,
-  `faqHeading` varchar(256) NOT NULL,
-  `faqContent` varchar(1024) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
---
--- Dumping data for table `faq`
---
-INSERT INTO `faq` (`faqID`, `faqHeading`, `faqContent`) VALUES
-(1, 'Is this all the faculty members?', '<p>No! The system will generate new information once there is a new faculty member that will be added by the admin(s)</p>'),
-(2, 'Can the students or general users edit information here?', '<p>Sadly no, only the faculty members or admins can edit the information on this website.</p>'),
-(3, 'When does the system updates new information?', '<p>The system will always be updated every new semester to keep up with the new informations or changes.</p>'),
-(4, 'How can I create an account?', '<p>You can create an account by filling up the form in the Registrar\'\s office. The admin will create the account for you with the password and assigned Role and those information will be emailed to you.</p>');
--- --------------------------------------------------------
---
--- Table structure for table `staff`
---
-CREATE TABLE `staff` (
-  `StaffNo` varchar(12) NOT NULL,
-  `Password` varchar(128) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
---
 -- Dumping data for table `staff`
 --
 INSERT INTO `staff` (`StaffNo`, `Password`) VALUES
 ('Admin', '12345');
 -- --------------------------------------------------------
---
--- Table structure for table `student`
---
-CREATE TABLE `student` (
-  `StudentNo` varchar(11) NOT NULL,
-  `Password` varchar(32) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 --
 -- Dumping data for table `student`
 --
@@ -85,13 +126,7 @@ INSERT INTO `student` (`StudentNo`, `Password`) VALUES
 ('student2', '12345'),
 ('Guest', '12345');
 -- --------------------------------------------------------
-CREATE TABLE `department` (
-  `DepartmentCode` varchar(256) NOT NULL,
-  `DepartmentName` varchar(256) NOT NULL,
-  `Email` varchar(256) NOT NULL,
-  `Phone` varchar(256) NOT NULL,
-  `Location` TEXT NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 --
 -- Dumping data for table `department`
 --
@@ -102,17 +137,8 @@ INSERT INTO `department` (`DepartmentCode`, `DepartmentName`, `Email`, `Phone`, 
 ('SBM', 'School of Business and Management', 'sas@edu.ph', '(032) 411 0308', 'Main Campus'),
 ('SOE', 'School of Engineering', 'soe@edu.ph', '(032) 406 7590', 'Main Campus'),
 ('SAMS', 'School of Allied Medical Sciences', 'sams@edu.ph', '(032) 401 1634', 'Main Campus');
---
--- Table structure for table `Faculty`
---
-CREATE TABLE `faculty` (
-  `FacultyID` varchar(10) NOT NULL,
-  `FacultyName` varchar(256) NOT NULL,
-  `Position` varchar(20) NOT NULL,
-  `Department` varchar(20) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `PhoneNumber` varchar(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
 --
 -- Dumping data for table `faculty`
 --
@@ -125,16 +151,8 @@ INSERT INTO `faculty` (`FacultyID`, `FacultyName`, `Position`, `Department`, `Em
 ('SCS 06', 'Josephine Petralba', 'Teacher', 'SCS', 'josephine.petralba@edu.ph', '09060133946'),
 ('SCS 07', 'Vicente Patalita III', 'Teacher', 'SCS', 'vicente.patalita@edu.ph', '09017033694'),
 ('SCS 08', 'Leeroy Gadiane', 'Teacher', 'SCS', 'leeroy.gadiane@edu.ph', '09087033964');
+-- --------------------------------------------------------
 
-
-CREATE TABLE `workHistory` (
-  `FacultyName` varchar(256) NOT NULL,
-  `CompanyName` varchar(256) NOT NULL,
-  `JobTitle` varchar(20) NOT NULL,
-  `StartDate` DATE NOT NULL,
-  `EndDate` DATE,
-  `Description` TEXT
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 --
 -- Dumping data for table `workHistory`
 --
@@ -147,15 +165,8 @@ INSERT INTO `workHistory` (`FacultyName`, `CompanyName`, `JobTitle`, `StartDate`
 ('Josephine Petralba', 'ABC Company', 'Software Developer', '2020-01-01', '2022-06-30', 'Worked on developing web applications using PHP and MySQL.'),
 ('Vicente Patalita III', 'ABC Company', 'Software Developer', '2020-01-01', '2022-06-30', 'Worked on developing web applications using PHP and MySQL.'),
 ('Leeroy Gadiane', 'ABC Company', 'Software Developer', '2020-01-01', '2022-06-30', 'Worked on developing web applications using PHP and MySQL.');
+-- --------------------------------------------------------
 
-
-CREATE TABLE `publications` (
-  `Title` varchar(256) NOT NULL,
-  `PublicationType` varchar(256) NOT NULL,
-  `PublicationDate` DATE NOT NULL,
-  `Author` varchar(256) NOT NULL,
-  `AuthorType` varchar(256) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 --
 -- Dumping data for table `publications`
 --
@@ -163,14 +174,8 @@ INSERT INTO `publications` (`Title`, `PublicationType`, `PublicationDate`, `Auth
 ('Advancements in Artificial Intelligence', 'Research Paper', '2021-01-01', 'Jovelyn Cuizon', 'First Author'),
 ('Data Mining Techniques', 'Book Chapter', '2024-01-01', 'Roderick Bandalan, Leeroy Gadiane', 'First Author, Co-Author'),
 ('Machine Learning Applications', 'Review Article', '2022-01-01', 'Josephine Petralba', 'Corresponding Author');
+-- --------------------------------------------------------
 
-
-CREATE TABLE `degrees` (
-  `FacultyName` varchar(256) NOT NULL,
-  `Degree` varchar(256) NOT NULL,
-  `DateAttained` DATE NOT NULL,
-  `Institution` varchar(256) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 --
 -- Dumping data for table `degrees`
 --
@@ -183,15 +188,10 @@ INSERT INTO `degrees` (`FacultyName`, `Degree`, `DateAttained`, `Institution`) V
 ('Josephine Petralba', 'M.S. in Computer Science', '2018-01-01', 'University of San-Jose Recoletos'),
 ('Vicente Patalita III', 'M.S. in Computer Science', '2018-01-01', 'Cebu Institute University'),
 ('Leeroy Gadiane', 'M.S. in Computer Science', '2018-01-01', 'University of San-Jose Recoletos');
+-- --------------------------------------------------------
 
-
-CREATE TABLE `grantsAwards` (
-  `FacultyName` varchar(256) NOT NULL,
-  `GrantsAwards` varchar(256) NOT NULL,
-  `DateAttained` DATE NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 --
--- Dumping data for table `degrees`
+-- Dumping data for table `grantsawards`
 --
 INSERT INTO `grantsAwards` (`FacultyName`, `GrantsAwards`, `DateAttained`) VALUES
 ('Jovelyn Cuizon', 'Best Paper Award', '2018-01-01'),
@@ -202,16 +202,10 @@ INSERT INTO `grantsAwards` (`FacultyName`, `GrantsAwards`, `DateAttained`) VALUE
 ('Josephine Petralba', 'Research Fellowship', '2018-01-01'),
 ('Vicente Patalita III', 'Outstanding Teaching Award', '2018-01-01'),
 ('Leeroy Gadiane', 'Outstanding Teaching Award', '2018-01-01');
+-- --------------------------------------------------------
 
-
-CREATE TABLE `courses` (
-  `CourseCode` varchar(256) NOT NULL,
-  `CourseName` varchar(256) NOT NULL,
-  `FacultyName` varchar(256) NOT NULL,
-  `Department` varchar(256) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 --
--- Dumping data for table `degrees`
+-- Dumping data for table `courses`
 --
 INSERT INTO `courses` (`CourseCode`, `CourseName`, `FacultyName`, `Department`) VALUES
 ('SA', 'Systems Administration and Maintenance', 'Jovelyn Cuizon', 'SCS'),
@@ -222,6 +216,18 @@ INSERT INTO `courses` (`CourseCode`, `CourseName`, `FacultyName`, `Department`) 
 ('Research', 'Methods of Research in Computing', 'Josephine Petralba', 'SCS'),
 ('DVA', 'Digital Visual Arts', 'Vicente Patalita III', 'SCS'),
 ('IT Review', '	Certification Exam Review', 'Leeroy Gadiane', 'SCS');
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `faq`
+--
+INSERT INTO `faq` (`faqID`, `faqHeading`, `faqContent`) VALUES
+(1, 'Is this all the faculty members?', '<p>No! The system will generate new information once there is a new faculty member that will be added by the admin(s)</p>'),
+(2, 'Can the students or general users edit information here?', '<p>Sadly no, only the faculty members or admins can edit the information on this website.</p>'),
+(3, 'When does the system updates new information?', '<p>The system will always be updated every new semester to keep up with the new informations or changes.</p>'),
+(4, 'How can I create an account?', '<p>You can create an account by filling up the form in the Registrar\'\s office. The admin will create the account for you with the password and assigned Role and those information will be emailed to you.</p>');
+-- --------------------------------------------------------
+--
 --
 -- Indexes for dumped tables
 --
@@ -274,6 +280,6 @@ ALTER TABLE `faq`
 COMMIT;
 
 -- select * from `roles`;
-select * from `student`;
-select * from `staff`;
-select * from `users`;
+-- select * from `student`;
+-- select * from `staff`;
+-- select * from `users`;
